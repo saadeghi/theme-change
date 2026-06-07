66e58c5d719901a852e51087462eb9278deaa45f
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2026-06-07 03:42:58 +0300

    fix: empty data-theme attribute. closes: #54

--- src/select.js
+++ src/select.js
@@ -1,26 +1,47 @@
 function themeSelect() {
   var selectEl = document.querySelector("select[data-choose-theme]");
-  var dataKey = selectEl ? selectEl.getAttribute('data-key') : null;
+  var dataKey = selectEl ? selectEl.getAttribute("data-key") : null;
   (function (theme = localStorage.getItem(dataKey ? dataKey : "theme")) {
     if (localStorage.getItem(dataKey ? dataKey : "theme")) {
       document.documentElement.setAttribute("data-theme", theme);
-      var optionToggler = document.querySelector("select[data-choose-theme] [value='" + theme.toString() + "']");
+      var optionToggler = document.querySelector(
+        "select[data-choose-theme] [value='" + theme.toString() + "']",
+      );
       if (optionToggler) {
-        [...document.querySelectorAll("select[data-choose-theme] [value='" + theme.toString() + "']")].forEach((el) => {
+        [
+          ...document.querySelectorAll(
+            "select[data-choose-theme] [value='" + theme.toString() + "']",
+          ),
+        ].forEach((el) => {
           el.selected = true;
         });
       }
     }
   })();
   if (selectEl) {
-    [...document.querySelectorAll("select[data-choose-theme]")].forEach((el) => {
-      el.addEventListener('change', function () {
-        document.documentElement.setAttribute("data-theme", this.value);
-        localStorage.setItem(dataKey ? dataKey : "theme", document.documentElement.getAttribute('data-theme'));
-        [...document.querySelectorAll("select[data-choose-theme] [value='" + localStorage.getItem(dataKey ? dataKey : "theme") + "']")].forEach((el) => {
-          el.selected = true;
+    [...document.querySelectorAll("select[data-choose-theme]")].forEach(
+      (el) => {
+        el.addEventListener("change", function () {
+          var selectedTheme = this.value;
+          if (selectedTheme === "") {
+            document.documentElement.removeAttribute("data-theme");
+            localStorage.removeItem(dataKey ? dataKey : "theme");
+          } else {
+            document.documentElement.setAttribute("data-theme", selectedTheme);
+            localStorage.setItem(dataKey ? dataKey : "theme", selectedTheme);
+          }
+          [...document.querySelectorAll("select[data-choose-theme]")].forEach(
+            (select) => {
+              var selectedOption = select.querySelector(
+                "[value='" + selectedTheme + "']",
+              );
+              if (selectedOption) {
+                selectedOption.selected = true;
+              }
+            },
+          );
         });
-      });
-    });
+      },
+    );
   }
 }
